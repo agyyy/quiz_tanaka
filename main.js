cc70e601e4b31885bf1a302e14a0902e039d6958
shintaro furuta <91622157+agyyy@users.noreply.github.com>
2022-01-02 16:23:40 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,96 +1,102 @@
+
 const quiz = [
     {
-        question: 'ゲーム市場、最も売れたゲーム機は次のうちどれ？',
-        answers: ['スーファミ','プレステ２','スイッチ','DS'],
-        correct: 'DS'
+        question: '田中の高校の地元はどこ？',
+        answers: ['東京','埼玉','茨城','神奈川','群馬'],
+        correct: '埼玉',
     },
     {
-        question: '糸井重里が企画に関わった、任天堂の看板ゲームといえば？',
-        answers: ['MOTHER2','スーパーマリオブラザーズ３','スーパードンキーコング','星のカービィ'],
-        correct: 'MOTHER2'
+        question: '井上の結婚式での村田の役職は何？',
+        answers: ['受付','スピーチ','カメラ','出し物','二次会幹事'],
+        correct: '受付',
     },
     {
-        question: 'ファイナルファンタジーIVの主人公の名前は？',
-        answers: ['フリオニール','クラウド','ティーダ','セシル'],
-        correct: 'セシル'
-    }
-];
+        question: '田中の奥さんはだれ？',
+        answers: ['白石ミカ','ゆみちゃん','まき','ななこ','みえこ'],
+        correct: 'ななこ',
+    },
+    {
+        question: '田中がオーストラリアで泥酔した際に介抱してくれたのはだれ？',
+        answers: ['まき','まきの浮気相手','デイビット','オリバー','ジャック'],
+        correct: 'デイビット',
+    },
+    {
+        question: '風間の車はどこ産？',
+        answers: ['イタリア','ドイツ','フランス','イギリス','アメリカ'],
+        correct: 'フランス',
+    },
+    {
+        question: '田中とななこが付き合いたて通っていたラブホの名前は？',
+        answers: ['HOTEL WHITE','HOTEL MOON','HOTEL LOVERS','HOTEL SWEET','HOTEL DREAM'],
+        correct: 'HOTEL LOVERS',
+    },
+    {
+        question: '田中がまきとのデートで注文したカクテルの名前は？',
+        answers: ['ジントニック','ハンター','ヘア・オブ・ザ・ドッグ','ピュア・ラブ','ハイ・ライフ'],
+        correct: 'ピュア・ラブ',
+    },
+]
+
+
+
 
 let quizIndex = 0;
-let quizLength = quiz.length;
-let score = 0;
+const $btn = document.getElementsByClassName('btn');
+const $qes = document.getElementById('js-question');
 
 
 
-
-// const question = 'ゲーム市場、最も売れたゲーム機は次のうちどれ？';
-// const answers = ['スーファミ','プレステ２','スイッチ','DS'];
-// const correct = 'DS';
-
-
-
-const $button = document.getElementsByTagName('button');
-
-// $button[0].textContent = answers[0];
-// $button[1].textContent = answers[1];
-// $button[2].textContent = answers[2];
-// $button[3].textContent = answers[3];
-
-//クイズの問題部、選択肢を定義
-const setupQuiz = ()=> {
-    document.getElementById('js-question').textContent = quiz[quizIndex].question;
-    let buttonIndex = 0;
-    while(buttonIndex<$button.length){
-    $button[buttonIndex].textContent = quiz[quizIndex].answers[buttonIndex];
-    buttonIndex++;
-}}
-
-setupQuiz();
-
-//クイズ正解不正解のif文
-const clickHandler = (e)=> {
-    console.log(e);
-
-    if(quiz[quizIndex].correct === e.target.textContent){
-        alert('正解！');
-        score++;
-    }else{
-        alert('不正解！');
-    }
-    
-    quizIndex++;
-    
-    if(quizIndex<quizLength){
-        setupQuiz();
-    }else{
-        alert('finish!あなたの正解数は'+score+'/'+quizLength+'です！');
-    }
-};
-
-let handleIndex = 0;
-console.log($button[handleIndex]);
-// console.log(handleIndex);
-while(handleIndex<$button.length){
-    $button[handleIndex].addEventListener('click',(e)=>{
-        clickHandler(e);
-    });
-    handleIndex++;
+const quizContent = function(){
+    $qes.textContent = quiz[quizIndex].question;
+    for(let index = 0; index < $btn.length; index++){
+        $btn[index].textContent = quiz[quizIndex].answers[index];
+}
 }
 
+quizContent();
+
+let score = 0;
+
+const quizFunction = function(){
+    for(let btnIndex = 0; btnIndex < $btn.length; btnIndex++){
+        $btn[btnIndex].addEventListener('click',(e) => {
+            if(e.target.textContent === quiz[quizIndex].correct){
+                alert('正解！！！')
+                score++;
+            }else{
+                alert('不正解...正解は'+quiz[quizIndex].correct+'でした。')
+            }
+            quizIndex++;
+            if(quizIndex < quiz.length){
+                quizContent();
+            }else{
+                alert('あなたの正解数は'+ score +'/7です！！');
+            }
+
+        })
+    } 
+}
+
+quizFunction();
 
 
 
-// $button[0].addEventListener('click',(e)=>{
-//     clickHandler(e);
-// });
-// $button[1].addEventListener('click',(e)=>{
-//     clickHandler(e);
-// });
-// $button[2].addEventListener('click',(e)=>{
-//     clickHandler(e);
-// });
-// $button[3].addEventListener('click',(e)=>{
-//     clickHandler(e);
-// });
 
-// console.log($button);
+
+
+
+class Yes {
+    constructor(nakami){
+        this.nakami = nakami;
+    }
+    uuu(price){
+        console.log('値段は'+price+'円です。');
+    }
+    nnn(){
+        console.log(this.nakami+'味です。')
+    }
+}
+
+let iii = new Yes('anko');
+iii.uuu(300);
+iii.nnn();
